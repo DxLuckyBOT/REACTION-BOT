1f72bac4b8cf148888946832389aad9377de77a8
DxLuckyBOT <cookie12552@gmail.com>
2024-12-02 04:05:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = "14de703801bf414bcffadfb59ae21fba"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
 OWNER_ID = int(getenv("OWNER_ID", "6458155947"))
-SUPPORT_GRP = "DX_INFO"
+SUPPORT_GRP = "DX_SOPORT"
 UPDATE_CHNL = "DxAbouT"
 OWNER_USERNAME = "Dx_LUCKY"
     
